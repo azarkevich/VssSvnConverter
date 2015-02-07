3209624048e010515b83d1fa2bf8adaf174614d0
Sergey Azarkevich <azarkevich@gmail.com>
2015-02-07 09:50:28 +0300

    Add one more mode for dump checkedout files - by checkout date

--- VssSvnConverter/LinksBuilder.cs
+++ VssSvnConverter/LinksBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using SourceSafeTypeLib;
 using vcslib;
 
@@ -11,9 +13,12 @@ namespace VssSvnConverter
 		public const string DataFileName = "3-state-links.txt";
 		public const string DataFileUiName = "3-state-links-ui.txt";
 		public const string DataFileCoName = "3-state-checkouts.txt";
+		public const string DataFileCoByDateName = "3-state-checkouts-by-date.txt";
 
 		public void Build(Options opts, IList<Tuple<string, int>> files)
 		{
+			var coDict = new Dictionary<DateTime, List<Tuple<string, string>>>();
+
 			var xrefsCo = new XRefMap();
 			var xrefs = new XRefMap();
 			foreach (var file in files.Select(t => t.Item1))
@@ -30,6 +35,16 @@ namespace VssSvnConverter
 					foreach (IVSSCheckout vssCheckout in vssLink.Checkouts)
 					{
 						xrefsCo.AddRef(item.Spec, vssCheckout.Username + " at " + vssCheckout.Date);
+
+						var coDate = vssCheckout.Date.Date;
+						List<Tuple<string, string>> list;
+						if(!coDict.TryGetValue(coDate, out list))
+						{
+							list = new List<Tuple<string, string>>();
+							coDict[coDate] = list;
+						}
+
+						list.Add(Tuple.Create(vssCheckout.Username, item.Spec));
 					}
 				}
 			}
@@ -37,6 +52,20 @@ namespace VssSvnConverter
 			xrefs.Save(DataFileName);
 			xrefs.Save(DataFileUiName, true);
 			xrefsCo.Save(DataFileCoName, true);
+
+			// save co dict by dates
+			using (var tw = File.CreateText(DataFileCoByDateName))
+			{
+				foreach (var kvp in coDict.OrderByDescending(kvp => kvp.Key))
+				{
+					tw.WriteLine("{0}", kvp.Key);
+					foreach (var tuple in kvp.Value)
+					{
+						tw.WriteLine("{0} at {1}", tuple.Item1, tuple.Item2);
+					}
+					tw.WriteLine();
+				}
+			}
 		}
 	}
 }
